b7552f0ac7f2a749851ae95804c6fd3261ed8baf
yu-heejin <jkgt0521@naver.com>
2023-03-27 22:37:33 +0900

    feat: [#3] jsoup 설정하기

--- src/main/java/com/techeeresc/tab/domain/shareinfo/service/ShareInfoService.java
+++ src/main/java/com/techeeresc/tab/domain/shareinfo/service/ShareInfoService.java
@@ -1,12 +1,14 @@
 package com.techeeresc.tab.domain.shareinfo.service;
 
+import org.jsoup.Connection;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShareInfoService {
-    @Scheduled(cron="0/10 * * * * *")
+    private final String WANTED = "https://www.wanted.co.kr/jobsfeed";
+    @Scheduled(cron="0 0 12 1/1 * *")
     public void crawlingScheduler() {
-        System.out.println("10초에 한번씩 실행");
+        Connection connection = Jsoup
     }
 }
